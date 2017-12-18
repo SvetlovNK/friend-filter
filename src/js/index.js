b46be92cc088bbbe8877397792a1d570dc772e5c
Nickolay Svetlov <ns@idaproject.com>
2017-12-18 13:17:24 +0300

    refactor code

--- src/js/index.js
+++ src/js/index.js
@@ -1,50 +1,54 @@
-const Handlebars = require('handlebars/dist/handlebars');
 const templateElement = require('../../filter-friend.hbs');
 
 export default function () {
     const allFriends = document.querySelector('.js-common-friends');
 
-    VK.init({
-        apiId: 6301314
-    });
-    
-    function authUser() {
-        return new Promise((resolve, reject) => {
-            VK.Auth.login(data => {
-                if (data.session) {
-                    resolve();
-                } else {
-                    reject(new Error('Не удалось авторизоваться'))
-                }
-            }, 2);
-        });
-    }
+    const filter = {
+        allFriends: undefined,
+        init: function () {
+            (async() => {
+               try {
+                   this.authentication();
+                   this.allFriends = await this.getFriends('friends.get', { fields: 'city,country, photo_100' });
+                   this.renderFriends(this.allFriends);
+               } catch (e) {
+                   console.error(e);
+               }
+            })();
+        },
+        authentication: function () {
+            return new Promise((resolve, reject) => {
+                VK.init({
+                    apiId: 6301314
+                });
 
-    function callAPI(method, params) {
-        params.v = '5.69';
+                VK.Auth.login(data => {
+                    if (data.session) {
+                        resolve();
+                    } else {
+                        reject(new Error('Не удалось авторизоваться'))
+                    }
+                }, 2);
+            })
+        },
+        getFriends: function (method, params) {
+            params.v = '5.69';
 
-        return new Promise((resolve, reject) => {
-            VK.api(method, params, (data) => {
-                if (!data.error) {
-                    resolve(data.response);
-                } else {
-                    reject(data.error);
-                }
-            });
-        })
-    }
-
-    (async() => {
-        try {
-            authUser();
-
-            const friends = await callAPI('friends.get', { fields: 'city,country, photo_100' });
+            return new Promise((resolve, reject) => {
+                VK.api(method, params, (data) => {
+                    if (!data.error) {
+                        resolve(data.response);
+                    } else {
+                        reject(data.error);
+                    }
+                });
+            })
+        },
+        renderFriends: function (friends) {
             const html = templateElement(friends);
-
             allFriends.innerHTML = html;
-
-        } catch (e) {
-            console.error(e);
         }
-    })();
+    };
+
+    filter.init();
 }
